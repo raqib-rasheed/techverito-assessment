eba50ada6a28960a2f628fcb4e73408ae1f06b1f
raqib <132666208+raqibabdulrasheed@users.noreply.github.com>
2023-09-11 01:16:57 +0530

    adding pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactElement } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Navbar, Sidebar, Footer, ScrollToTop } from "./components";
+
+import {
+  Home,
+  Error,
+  Shipping,
+  SingleProduct,
+  Checkout,
+  Products,
+  Cart,
+  SuccessfulPayment,
+} from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/shipping" element={<Shipping />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:slug" element={<SingleProduct />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route
+            path="/successful_payment"
+            element={<SuccessfulPayment />}
+          />
+          <Route path="*" element={<Error />}></Route>
+        </>
+    </Routes>
   );
 }
 
 export default App;
+
+const Layout: React.FC<{ children: ReactElement }> = ({ children }) => {
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Sidebar />
+      {children}
+      <Footer />
+    </>
+  );
+};
